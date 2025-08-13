302e9ab094c43daeabf4f3ceebba936406913f2b
Rodrigo <rodrigo.rojas.g@usach.cl>
2025-08-12 21:15:34 -0400

    Primer commit

---
+++ src/main/java/com/example/conversor/controller/ConversorController.java
@@ -0,0 +1,16 @@
+package com.example.conversor.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/conversor")
+@RestController
+public class ConversorController {
+
+    @GetMapping("/convertToBinary/{number}")
+    public String convertToBinary(@PathVariable int number) {
+        return Integer.toBinaryString(number);
+    }
+}
